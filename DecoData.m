a99f74abc956a613ff305cf6515ccfeccf704d86
hanyu-song <hanyu.song@duke.edu>
2017-03-02 17:25:40 -0500

    removed files & saved them in folders instaed"
    :q1
    :q
    git push
    
    :q

--- DecoData.m
+++
@@ -1,9 +0,0 @@
-function [deco_y, deco_x] = DecoData(y,x)
-p = size(x,2);
-[U, D] = eig(x * x');
-robustTerm = 1; % consider make it an input
-F = sqrt(p) * U * diag(1./sqrt((diag(D) + robustTerm))) * U';
-deco_y = F * y;
-deco_x = F * x;
-end
-
